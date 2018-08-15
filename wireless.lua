5503811048b8407e37af1529beb39a54695803a7
Jon Sangster <jon@ertt.ca>
2018-08-15 04:52:32 -0700

    reindent

--- wireless.lua
+++ wireless.lua
@@ -13,15 +13,15 @@ local function worker(args)
     local connected = false
 
     -- Settings
-    local ICON_DIR      = awful.util.getdir("config").."/"..module_path.."/net_widgets/icons/"
+    local ICON_DIR      = awful.util.getdir("config").."/"..module_path.."/net-widgets/icons/"
     local interface     = args.interface or "wlan0"
     local timeout       = args.timeout or 5
     local font          = args.font or beautiful.font
     local popup_signal  = args.popup_signal or false
     local popup_position = args.popup_position or naughty.config.defaults.position
     local onclick       = args.onclick
-    local widget 	= args.widget == nil and wibox.layout.fixed.horizontal() or args.widget == false and nil or args.widget
-    local indent 	= args.indent or 3
+    local widget     = args.widget == nil and wibox.layout.fixed.horizontal() or args.widget == false and nil or args.widget
+    local indent     = args.indent or 3
 
     local net_icon = wibox.widget.imagebox()
     net_icon:set_image(ICON_DIR.."wireless_na.png")
@@ -30,8 +30,8 @@ local function worker(args)
     net_text:set_text(" N/A ")
     local signal_level = 0
     local function net_update()
-	awful.spawn.easy_async("awk 'NR==3 {printf \"%3.0f\" ,($3/70)*100}' /proc/net/wireless", function(stdout, stderr, reason, exit_code)
-          signal_level = tonumber( stdout )
+        awful.spawn.easy_async("awk 'NR==3 {printf \"%3.0f\" ,($3/70)*100}' /proc/net/wireless", function(stdout, stderr, reason, exit_code)
+            signal_level = tonumber( stdout )
         end)
         if signal_level == nil then
             connected = false
@@ -54,52 +54,52 @@ local function worker(args)
 
     net_update()
     local timer = gears.timer.start_new( timeout, function () net_update()
-      return true end )
+        return true end )
 
-    widgets_table["imagebox"]	= net_icon
-    widgets_table["textbox"]	= net_text
-    if widget then
-	    widget:add(net_icon)
-	    -- Hide the text when we want to popup the signal instead
-	    if not popup_signal then
-		    widget:add(net_text)
-	    end
-	    wireless:attach(widget,{onclick = onclick})
-    end
-
-
-
-    local function text_grabber()
-        local msg = ""
-        if connected then
-            local mac     = "N/A"
-            local essid   = "N/A"
-            local bitrate = "N/A"
-            local inet    = "N/A"
-
-            -- Use iw/ip
-            f = io.popen("iw dev "..interface.." link")
-            for line in f:lines() do
-                -- Connected to 00:01:8e:11:45:ac (on wlp1s0)
-                mac     = string.match(line, "Connected to ([0-f:]+)") or mac
-                -- SSID: 00018E1145AC
-                essid   = string.match(line, "SSID: (.+)") or essid
-                -- tx bitrate: 36.0 MBit/s
-                bitrate = string.match(line, "tx bitrate: (.+/s)") or bitrate
+        widgets_table["imagebox"]    = net_icon
+        widgets_table["textbox"]    = net_text
+        if widget then
+            widget:add(net_icon)
+            -- Hide the text when we want to popup the signal instead
+            if not popup_signal then
+                widget:add(net_text)
             end
-            f:close()
+            wireless:attach(widget,{onclick = onclick})
+        end
 
-            f = io.popen("ip addr show "..interface)
-            for line in f:lines() do
-                inet    = string.match(line, "inet (%d+%.%d+%.%d+%.%d+)") or inet
-            end
-            f:close()
 
-            signal = ""
-            if popup_signal then
-                signal = "├Strength\t"..signal_level.."\n"
-            end
-            msg =
+
+        local function text_grabber()
+            local msg = ""
+            if connected then
+                local mac     = "N/A"
+                local essid   = "N/A"
+                local bitrate = "N/A"
+                local inet    = "N/A"
+
+                -- Use iw/ip
+                f = io.popen("iw dev "..interface.." link")
+                for line in f:lines() do
+                    -- Connected to 00:01:8e:11:45:ac (on wlp1s0)
+                    mac     = string.match(line, "Connected to ([0-f:]+)") or mac
+                    -- SSID: 00018E1145AC
+                    essid   = string.match(line, "SSID: (.+)") or essid
+                    -- tx bitrate: 36.0 MBit/s
+                    bitrate = string.match(line, "tx bitrate: (.+/s)") or bitrate
+                end
+                f:close()
+
+                f = io.popen("ip addr show "..interface)
+                for line in f:lines() do
+                    inet    = string.match(line, "inet (%d+%.%d+%.%d+%.%d+)") or inet
+                end
+                f:close()
+
+                signal = ""
+                if popup_signal then
+                    signal = "├Strength\t"..signal_level.."\n"
+                end
+                msg =
                 "<span font_desc=\""..font.."\">"..
                 "┌["..interface.."]\n"..
                 "├ESSID:\t\t"..essid.."\n"..
@@ -109,47 +109,47 @@ local function worker(args)
                 "└Bit rate:\t"..bitrate.."</span>"
 
 
-        else
-            msg = "Wireless network is disconnected"
+            else
+                msg = "Wireless network is disconnected"
+            end
+
+            return msg
         end
 
-        return msg
+        local notification = nil
+        function wireless:hide()
+            if notification ~= nil then
+                naughty.destroy(notification)
+                notification = nil
+            end
+        end
+
+        function wireless:show(t_out)
+            wireless:hide()
+
+            notification = naughty.notify({
+                preset = fs_notification_preset,
+                text = text_grabber(),
+                timeout = t_out,
+                screen = mouse.screen,
+                position = popup_position
+            })
+        end
+        return widget or widgets_table
     end
 
-    local notification = nil
-    function wireless:hide()
-	    if notification ~= nil then
-		    naughty.destroy(notification)
-		    notification = nil
-	    end
+    function wireless:attach(widget, args)
+        local args = args or {}
+        local onclick = args.onclick
+        -- Bind onclick event function
+        if onclick then
+            widget:buttons(awful.util.table.join(
+            awful.button({}, 1, function() awful.util.spawn(onclick) end)
+            ))
+        end
+        widget:connect_signal('mouse::enter', function () wireless:show(0) end)
+        widget:connect_signal('mouse::leave', function () wireless:hide() end)
+        return widget
     end
 
-    function wireless:show(t_out)
-	    wireless:hide()
-
-	    notification = naughty.notify({
-		    preset = fs_notification_preset,
-		    text = text_grabber(),
-		    timeout = t_out,
-            screen = mouse.screen,
-            position = popup_position
-	    })
-    end
-    return widget or widgets_table
-end
-
-function wireless:attach(widget, args)
-    local args = args or {}
-    local onclick = args.onclick
-    -- Bind onclick event function
-    if onclick then
-	    widget:buttons(awful.util.table.join(
-	    awful.button({}, 1, function() awful.util.spawn(onclick) end)
-	    ))
-    end
-    widget:connect_signal('mouse::enter', function () wireless:show(0) end)
-    widget:connect_signal('mouse::leave', function () wireless:hide() end)
-    return widget
-end
-
-return setmetatable(wireless, {__call = function(_,...) return worker(...) end})
+    return setmetatable(wireless, {__call = function(_,...) return worker(...) end})
